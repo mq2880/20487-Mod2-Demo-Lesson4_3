328e7e36685cd0fd8c18dbec1e1dd3952d9c939f
Javier Moreno <mq2880@gmail.com>
2021-01-14 11:07:49 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Module 2: Querying and Manipulating Data Using Entity Framework
+
+# Lesson 4: Manipulating Data
+
+### Demonstration: Using Entity Framework with SQLite
+
+- Abrimos el proyecto de consola Sqlite en Visual Studio Code 
+- Añadimos el paquete Microsoft.EntityFrameworkCore.Sqlite
+- En la clase **DBSqliteTest** del subproyecto **Sqlite.Dal.Test** añadimos la propiedad **_options** 
+- Ejecutar
+
+
+
+![](./img/Captura1.jpg)
+
+**DB Browser for SQLite**
+
+![](./img/Captura2.jpg)
---
+++ Starter/Sqlite.Dal.Test/DBSqliteTest.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sqlite.Dal.Database;
+using Microsoft.EntityFrameworkCore;
+using Sqlite.Dal.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Sqlite.Dal.Repository;
+
+namespace Sqlite.Dal.Test
+{
+    [TestClass]
+    public class DBSqliteTest
+    {
+        private StudentRepository _studentRepository;
+        private TeacherRepository _teacherRepository;
+        private DbContextOptions<SchoolContext> _options =
+             new DbContextOptionsBuilder<SchoolContext>()
+                 .UseSqlite(@"Data Source = D:\20487C\Demo\demo2Lesson4_3\Database\SqliteSchool.db")
+                 .Options;
+
+        public DBSqliteTest()
+        {
+            _studentRepository = new StudentRepository(_options);
+            _teacherRepository = new TeacherRepository(_options);
+        }
+
+        [TestMethod]
+        public void AddStudentTest()
+        {
+            Student student = new Student { Name = "Kari Hensien" };
+            student = _studentRepository.Add(student);
+
+            using (var context = new SchoolContext(_options))
+            {
+                var result = context.Students.FirstOrDefault((s) => s.PersonId == student.PersonId);
+                Assert.IsNotNull(result);
+            }
+        }
+    }
+}
